ba5d2459b3fd94600d4316caf7f41e049af064e4
Travis Hoppe <travis.hoppe@gmail.com>
2021-09-17 16:38:21 -0400

    Version bump

--- nans_are_numbers/_version.py
+++ nans_are_numbers/_version.py
@@ -1,6 +1,7 @@
-__version__ = "1.0.2"
+__version__ = "1.0.3"
 
 """
+Version: 1.0.3: Fix badge link in README.
 Version: 1.0.2: Fix pypi link.
 Version: 1.0.1: Experimenting with pypi settings.
 Version: 1.0.0: First release.
